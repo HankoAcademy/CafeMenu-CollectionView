95ac00907492cf6519b6d23f52c3c3eacaae928a
Han Kim <hanniek00@gmail.com>
2021-09-27 10:48:21 -0500

    Add image assets, color assets, and starter Xcode project

---
+++ CafeMenu-CollectionView/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CafeMenu-CollectionView
+//
+//  Created by Hannie Kim on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
