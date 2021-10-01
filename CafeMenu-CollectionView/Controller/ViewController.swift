9dd4f6df9479b46baa0f4e5af2599088bf1dd1f2
vpark777 <vkphottie@gmail.com>
2021-10-01 10:14:52 -0700

    just pull request test w text change

--- CafeMenu-CollectionView/Controller/ViewController.swift
+++ CafeMenu-CollectionView/Controller/ViewController.swift
@@ -3,7 +3,8 @@
 //  CafeMenu-CollectionView
 //
 //  Created by Hannie Kim on 9/24/21.
-//
+//  Homework done by Victoria - this version is for testing
+//  making pull request properly
 
 import UIKit
 
